4418714c615e5cce97e4813d19f968d8b6b8aad2
stian-git <82411646+stian-git@users.noreply.github.com>
2021-12-14 20:15:31 +0100

    IDC-19 - Retrieve URLParams on confirmation page.

--- js/checkout-success.js
+++ js/checkout-success.js
@@ -1,2 +1,25 @@
-// Make sure we clear the storage when order has been placed.
+// Make sure we clear the storage when order has been placed:
 storage.clear();
+
+// Retrieve parameters from URL:
+
+const queries = document.location.search;
+const params = new URLSearchParams(queries);
+
+const customerName = params.get("name");
+const customerEmail = params.get("email");
+const orderNumber = 123456;
+//console.log("Thank you, " + customerName);
+
+//const nameContainer = document.querySelector(".nameContainer");
+
+//nameContainer.innerHTML = `, ${customerName}`;
+
+const checkout_textContainer = document.querySelector(".checkout__main");
+
+checkout_textContainer.innerHTML = `
+<p>Thank you for placing your order, <i>${customerName}</i>. (order number: ${orderNumber}).</p>
+<p>We have successfully received your order and an order confirmation will be sent to <i>${customerEmail}</i> shortly.</p>
+<p>To trace the status of you order, please follow the instructions from the order confirmation.</p>
+<p class="checkout_punchline">Pushing the comfort zone.</p>
+`;
